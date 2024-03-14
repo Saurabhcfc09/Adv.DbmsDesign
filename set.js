133d2e31d4d04917fbc25962c9dc2f25cc3794d7
Saurabhcfc09 <157640196+Saurabhcfc09@users.noreply.github.com>
2024-03-13 21:23:49 -0400

    Create set.js

---
+++ set.js
@@ -0,0 +1,12 @@
+@@ -0,0 +1,12 @@
+db.zips.aggregate([
+    {
+        $project: {
+        state:1,
+        zip:1,
+        population:"$pop",
+        pop_2022: { $round: { $multiply: [1.0031, "$pop"]}},
+        _id:0
+    }
+    }
+    ])
